de98ecd133dfb4bfee053d56ead04bda5e1e75fb
OulaNasra <eng.oula.nasra.94@gmail.com>
2021-01-30 16:57:56 +0200

    The 8 commit -Styling views with bootstrap

--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -6,18 +6,37 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
    
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
        
         <title>Todos</title>
      </head>
      
      <body>
-          <h1>TODOS PAGE </h1>
-          <ul>
+<div class="container">
+<h1 class="text-center my-5">TODOS PAGE </h1>
+     <div class="row justify-content-center">
+     <div class="col-md-8 ">
+     <div class="card card-default">
+          <div class="card card-header">
+          Todos
+          </div>
+          <div class="card-body">
+          <ul class ="list-group">
           @foreach($todos as $todo)
-          <li>
+          <li class ="list-group-item">
           {{$todo->name}}
+       <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+  <button class="btn btn-success me-md-2" type="button">Edit</button>
+  <button class="btn btn-danger" type="button">Delete</button>
+</div>
           </li>
           @endforeach
           </ul>
+          </div>
+          </div>
+</div>
+          
+     </div>
+     </div>
      </body>
  </html>
