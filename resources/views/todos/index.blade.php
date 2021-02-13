6b1978909716dc78c1304b1fb876cb63377bd836
OulaNasra <eng.oula.nasra.94@gmail.com>
2021-02-13 11:39:29 +0200

    The 10 commit -Introduction to layout files

--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -1,19 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-   
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-       
-        <title>Todos</title>
-     </head>
-     
-     <body>
-       <div class="container">
-       <h1 class="text-center my-5">TODOS PAGE </h1>
+@extends('layouts.app')
+@section('title')
+Todos List
+@endsection
+@section('content')
+<h1 class="text-center my-5">TODOS PAGE </h1>
        <div class="row justify-content-center">
         <div class="col-md-8 ">
          <div class="card card-default">
@@ -27,7 +17,7 @@
            {{$todo->name}}
        <div class="d-grid gap-2 d-md-flex justify-content-md-end">
   <a href="/todos/{{$todo->id}}" class="btn btn-success me-md-2" type="button">Edit</a>
-  <button class="btn btn-danger" type="button">Delete</button>
+  <a href="/todos/{{$todo->id}}" class="btn btn-danger " type="button">Delete</a>
 </div>
           </li>
           @endforeach
@@ -37,6 +27,4 @@
 </div>
           
      </div>
-     </div>
-     </body>
- </html>
+@endsection
