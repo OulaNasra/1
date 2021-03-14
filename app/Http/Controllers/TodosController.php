faf49c1e86d2b9112a3110a0a3b2589e95aba99a
OulaNasra <eng.oula.nasra.94@gmail.com>
2021-03-14 09:16:15 +0200

    The 14 commit -Deleting todos

--- app/Http/Controllers/TodosController.php
+++ app/Http/Controllers/TodosController.php
@@ -62,4 +62,11 @@ class TodosController extends Controller
             $todo -> save();
             return redirect('/todos');
         }
+
+            public function destroy($todoId)
+            {
+               $todo=Todo::find($todoId);
+               $todo->delete();
+               return redirect('/todos');
+            }
 }
