443f0421f6e36e1cba609f4469132ea079cac68f
OulaNasra <eng.oula.nasra.94@gmail.com>
2021-03-16 09:38:47 +0200

    The 15 commit -Introducing route model binding

--- app/Http/Controllers/TodosController.php
+++ app/Http/Controllers/TodosController.php
@@ -11,12 +11,12 @@ class TodosController extends Controller
      {
         return view ('todos.index')->with('todos',Todo::all());
      }
-    public function show($todoId)
+    public function show(Todo $todo)
      {
-        return view ('todos.show')->with('todo',Todo::find($todoId));
+        return view ('todos.show')->with('todo',$todo);
      }
 
-    public function create ()
+   public function create ()
      {
         return view ('todos.create');
      }
@@ -41,13 +41,12 @@ class TodosController extends Controller
         return redirect('/todos');
      }
 
-       public function edit ($todoId)
+       public function edit (Todo $todo)
         {
-           $todo=Todo::find($todoId);
            return view ('todos.edit')->with('todo',$todo);
         }
 
-        public function update($todoId)
+        public function update(Todo $todo)
 
         {
          $this->validate(request(),[
@@ -56,16 +55,16 @@ class TodosController extends Controller
             ]);
     
             $data = request()->all();
-            $todo =Todo::find($todoId);
+            
             $todo -> name=$data['name'];
             $todo -> description = $data['description'];
             $todo -> save();
             return redirect('/todos');
         }
 
-            public function destroy($todoId)
+            public function destroy(Todo $todo)
             {
-               $todo=Todo::find($todoId);
+            
                $todo->delete();
                return redirect('/todos');
             }
